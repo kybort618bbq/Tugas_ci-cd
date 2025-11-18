4d3ea15f407903fdd116bc73d27c0b724814b425
RTS.Dewi Afriyani <studyedd337@gmail.com>
2025-11-19 00:43:57 +0700

    Fix MySQL config + CI

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -2,16 +2,18 @@
 
 class DatabaseTest
 {
-    public $conn; // UBah protected menjadi PUBLIC
+    public $conn;
 
     public function __construct()
     {
-        $host = getenv("DB_HOST");
+        $host = getenv("DB_HOST") ?: "127.0.0.1";   // gunakan host benar di GitHub
         $name = getenv("DB_NAME");
         $user = getenv("DB_USER");
         $pass = getenv("DB_PASS");
 
-        $this->conn = new PDO("mysql:host=$host;dbname=$name", $user, $pass);
+        $dsn = "mysql:host=$host;dbname=$name;charset=utf8";
+
+        $this->conn = new PDO($dsn, $user, $pass);
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 }
